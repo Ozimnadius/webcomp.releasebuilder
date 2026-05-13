126a2620e422b9df86acfb454cfc4ab32f5e210b
Mikle Seregin <mikhail-seregin@yandex.ru>
2026-05-13 12:45:59 +0300

    feat: add vendor configuration (partner name, uri, vendor.php)

---
+++ install/index.php
@@ -0,0 +1,63 @@
+<?php
+
+use Bitrix\Main\Localization\Loc;
+
+Loc::loadMessages(__FILE__);
+
+class webcomp_releasebuilder extends CModule
+{
+    public $MODULE_ID = 'webcomp.releasebuilder';
+    public $MODULE_VERSION;
+    public $MODULE_VERSION_DATE;
+    public $MODULE_NAME;
+    public $MODULE_DESCRIPTION;
+    public $PARTNER_NAME;
+    public $PARTNER_URI;
+
+    const partnerName  = 'webcomp';
+    const solutionName = 'releasebuilder';
+
+    public function __construct()
+    {
+        $this->MODULE_NAME        = Loc::getMessage('WEBCOMP_RELEASEBUILDER_MODULE_NAME');
+        $this->MODULE_DESCRIPTION = Loc::getMessage('WEBCOMP_RELEASEBUILDER_MODULE_DESC');
+        $this->PARTNER_NAME       = Loc::getMessage('WEBCOMP_RELEASEBUILDER_PARTNER_NAME');
+        $this->PARTNER_URI        = Loc::getMessage('WEBCOMP_RELEASEBUILDER_PARTNER_URI');
+
+        $this->SHOW_SUPER_ADMIN_GROUP_RIGHTS = 'Y';
+        $this->MODULE_GROUP_RIGHTS           = 'Y';
+
+        include __DIR__ . '/version.php';
+
+        if (isset($arModuleVersion['VERSION'])) {
+            $this->MODULE_VERSION      = $arModuleVersion['VERSION'];
+            $this->MODULE_VERSION_DATE = $arModuleVersion['VERSION_DATE'] ?? null;
+        }
+    }
+
+    public function DoInstall(): void
+    {
+        $this->InstallFiles();
+        RegisterModule($this->MODULE_ID);
+    }
+
+    public function DoUninstall(): void
+    {
+        $this->UnInstallFiles();
+        UnRegisterModule($this->MODULE_ID);
+    }
+
+    public function InstallFiles(): void
+    {
+        CopyDirFiles(__DIR__ . '/admin', $_SERVER['DOCUMENT_ROOT'] . '/bitrix/admin', true);
+        CopyDirFiles(__DIR__ . '/js',  $_SERVER['DOCUMENT_ROOT'] . '/bitrix/js/'  . $this->MODULE_ID, true);
+        CopyDirFiles(__DIR__ . '/css', $_SERVER['DOCUMENT_ROOT'] . '/bitrix/css/' . $this->MODULE_ID, true);
+    }
+
+    public function UnInstallFiles(): void
+    {
+        DeleteDirFiles(__DIR__ . '/admin', $_SERVER['DOCUMENT_ROOT'] . '/bitrix/admin');
+        DeleteDirFiles(__DIR__ . '/js',  $_SERVER['DOCUMENT_ROOT'] . '/bitrix/js/'  . $this->MODULE_ID);
+        DeleteDirFiles(__DIR__ . '/css', $_SERVER['DOCUMENT_ROOT'] . '/bitrix/css/' . $this->MODULE_ID);
+    }
+}
---
+++ lang/ru/install/index.php
@@ -0,0 +1,5 @@
+<?php
+$MESS['WEBCOMP_RELEASEBUILDER_MODULE_NAME'] = 'Конструктор релизов';
+$MESS['WEBCOMP_RELEASEBUILDER_MODULE_DESC'] = 'Инструмент для сборки архивов обновлений вендорных модулей.';
+$MESS['WEBCOMP_RELEASEBUILDER_PARTNER_NAME'] = 'WEB-COMP - создание и продвижение сайтов';
+$MESS['WEBCOMP_RELEASEBUILDER_PARTNER_URI']  = 'https://web-komp.ru/';
