4abbd1a26b4f9655933a91a72f697157503312c7
Mikle Seregin <mikhail-seregin@yandex.ru>
2026-05-13 16:38:36 +0300

    chore: bump version to 0.0.1, add alpha warning to README

--- install/version.php
+++ install/version.php
@@ -1,5 +1,5 @@
 <?php
 $arModuleVersion = [
-    'VERSION'      => '1.0.0',
-    'VERSION_DATE' => '2026-05-12 12:00:00',
+    'VERSION'      => '0.0.1',
+    'VERSION_DATE' => '2026-05-13 16:30:00',
 ];
